cc71007947d0e8919aabbacb957223fb5f534607
Mylena Vitoriano <mylenabarbosa08@gmail.com>
2022-02-11 21:18:25 -0300

    realizado a integração do banco de dados com recursos do php para trazer dados e enviá-lo

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
+<?php
+session_start();
+include_once 'sis/functions.php';
+$nomeUser = $_SESSION['nome'];
+$id_usuario = $_SESSION['id'];
+
+
+$tarefas = buscar_tarefas_user($id_usuario);
+
+function verifica_finish($x, $elemento){
+  if($x == 1 && $elemento == "class"){
+    //echo "oioioi";
+    echo "finished";
+  }else if($x == 1 && $elemento == "check"){
+    //echo "aaaa";
+    echo "checked";
+  }else{
+    //echo "cccc";
+    echo "";
+  }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -24,7 +47,7 @@
       </a>
 
       <div class="user" id="user">
-        Usuário
+        <?=$nomeUser;?>
         <img src="images/vector.svg" alt="Ícone usuário">
       </div>
     </header>
@@ -33,58 +56,83 @@
 
       <section id="tasks">
         <div class="tasks">
-          <div class="task-wrapper finished">
-            <div class="task-content">
-              <input type="checkbox" class="checkbox-task">
+          <?php foreach($tarefas as $tarefa){?>
+            <div class="task-wrapper <?php verifica_finish($tarefa['concluida'], "class");?>" id_tarefa="<?=$tarefa['id'];?>">
+              <div class="task-content">
+                <input type="checkbox" class="checkbox-task" id_tarefa="<?=$tarefa['id'];?>" <?php verifica_finish($tarefa['concluida'], "check");?>>
+              </div>
+              <div class="task-text">
+                <p><?=$tarefa['tarefa'];?></p>
+              </div>
+              <div class="delete-task">
+                <button><img src="images/trash.svg"></button>
+              </div>
             </div>
-            <div class="task-text">
-              <p>Tarefa W</p>
-            </div>
-            <div class="delete-task">
-              <button><img src="images/trash.svg"></button>
-            </div>
-          </div>
-
-          <div class="task-wrapper">
-            <div class="task-content">
-              <input type="checkbox" class="checkbox-task">
-            </div>
-            <div class="task-text">
-              <p>Tarefa X</p>
-            </div>
-            <div class="delete-task">
-              <button><img src="images/trash.svg"></button>
-            </div>
-          </div>
-
-          <div class="task-wrapper">
-            <div class="task-content">
-              <input type="checkbox" class="checkbox-task">
-            </div>
-            <div class="task-text">
-              <p>Tarefa Y</p>
-            </div>
-            <div class="delete-task">
-              <button><img src="images/trash.svg"></button>
-            </div>
-          </div>
-
-          
+          <?php }?>
         </div>        
       </section>
 
       <section id="new-tasks">
         <div class="form">
-          <form action="">
+          <div class="inputs">
+            <input type="hidden" id="id_usuario" value="<?=$id_usuario;?>">
             <input type="text" name="task" id="task" placeholder="Escreva sua tarefa...">
 
             <footer>
-              <button><img src="images/send.svg" alt="Ícone enviar"></button>
+              <button id="btn_send"><img src="images/send.svg" alt="Ícone enviar"></button>
             </footer>
-          </form>
+          </div>
         </div>         
       </section>
     </main>
   </div>
+
+
+    
+  <script src="js/jquery-3.6.0.min.js"></script>
+
+
+  <script>
+    $("#btn_send").click(function(){
+      if($("#task").val() == ""){
+        alert("Preencha o campo de tarefa!");
+      }else{
+        $.post('sis/functions.php', {
+          tarefa: $("#task").val(),
+          usuario: $("#id_usuario").val(),
+          action: 'nova_tarefa'
+        }).done(function(data){
+          dados = JSON.parse(data);
+          $("#task").val("");
+          $(".tasks").prepend(
+            "<div class='task-wrapper'>\n\
+              <div class='task-content'>\n\
+                <input type='checkbox' class='checkbox-task' id_tarefa='"+dados[0]['id']+"'>\n\
+              </div>\n\
+              <div class='task-text'>\n\
+                <p>"+dados[0]['tarefa']+"</p>\n\
+              </div>\n\
+              <div class='delete-task'>\n\
+                <button><img src='images/trash.svg'></button>\n\
+              </div>\n\
+            </div>"
+          );
+
+        });
+      }
+    });
+
+    $(".checkbox-task").click(function(){
+      $(this).parent().parent().toggleClass("finished");
+      $.post('sis/functions.php', {
+        id_tarefa:  $(this).attr("id_tarefa"),
+        action: 'check_tarefa'
+      }).done(function(data){
+        console.log(data);
+      });
+    });
+
+  </script>
+
 </body>
 </html>
